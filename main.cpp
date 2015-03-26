a4933905519c5a9082a34b75786af963b2c6f92b
Turupawn <turupawn@nya>
2015-03-26 11:04:09 -0600

    primer commit

---
+++ main.cpp
@@ -0,0 +1,84 @@
+// queue::push/pop
+
+#include <iostream>
+#include <list>
+#include <fstream>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include "Planeta.h"
+#include "Evaluador.h"
+#include "NodoBinario.h"
+
+using namespace std;
+
+//Escribe los datos del objeto mascota dado en un archivo binario con el nombre dado en la posicion dada
+//Nota: Se deben poder agregar varias mascotas en un solo archivo
+void escribir(string nombre_archivo, Planeta*planeta, int posicion)
+{
+}
+
+//Devuelve una mascota previamente escrita (con la funcion escribir()) en un archivo binario con nombre dado en la posicion dada
+//Nota: Se deben poder leer varias mascotas de un solo archivo
+Planeta* leer(string nombre_archivo, int posicion)
+{
+    Planeta *p = new Planeta("", -1, -1.0, ' ');
+    return p;
+}
+
+//Devuelve el primer valor fue ingresado en mi_pila (dada)
+//Nota: devuelve el valor mas viejo
+string getFondo(stack<string> mi_pila)
+{
+    string resultado;
+    return resultado;
+}
+
+//Devuelve el ultimo valor que fue ingresado en mi_cola (dada)
+//Nota: devuelve el valor mas nuevo
+char getUltimo(queue<char> mi_cola)
+{
+    char resultado;
+    return resultado;
+}
+
+//Dado una lista, devuelve un vector que contenga los datos de la lista en el mismo orden
+vector<int> convertirAVector(list<int> mi_lista)
+{
+    vector<int> mi_vector;
+    return mi_vector;
+}
+
+//Dado un numero, devuelve un mapa que contenga su tabla de multiplicacion
+//Ejemplo:
+//Dado num = 5
+//Devuelve el siguiente mapa
+//{1=>5,2=>10,3=>15,4=>20,5=>25,6=>30,7=>35,8=>40,9=>45,10=>50}
+map<int,int> getTabla(int num)
+{
+    map<int,int> mi_mapa;
+    return mi_mapa;
+}
+
+//Dado un arbol con raiz dada, devuelve un set que contenga todos los valores almacenados en el arbol
+//Nota los valores estan almacendados en el atributo valor en la clase NodoBinario
+set<string> getValores(NodoBinario* raiz)
+{
+    set<string> mi_set;
+    return mi_set;
+}
+
+//Dado un arbol con raiz dada, busca el nodo que contenga el valor buscado (dado) y lo reemplaza con la variable reemplazo (dada)
+//Nota los valores estan almacendados en el atributo valor en la clase NodoBinario
+void buscarYReemplazar(NodoBinario* raiz, string buscado, string reemplazo)
+{
+}
+
+int main ()
+{
+    //Funcion evaluadora
+    evaluar();
+    return 0;
+}
+
